a8faddd500610c79e77239142cca5deb42463db0
Aapo Laivamaa <aapolai@uef.fi>
2022-04-11 15:52:30 +0300

    nappi lisätty

--- Mökkivaraus/Form1.Designer.cs
+++ Mökkivaraus/Form1.Designer.cs
@@ -47,6 +47,7 @@ namespace Mökkivaraus
             this.asiakasBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.asiakasBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.vnDataSetBindingSource)).BeginInit();
@@ -152,6 +153,7 @@ namespace Mökkivaraus
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
@@ -167,6 +169,15 @@ namespace Mökkivaraus
             this.label1.Size = new System.Drawing.Size(60, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Testilaabeli";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "jou";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -208,6 +219,7 @@ namespace Mökkivaraus
         private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn puhelinnroDataGridViewTextBoxColumn;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
