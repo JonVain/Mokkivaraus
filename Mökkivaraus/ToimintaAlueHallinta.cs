e6e350f3d84faf8c3d6c8472660f65901872a991
Aapo Laivamaa <aapolai@uef.fi>
2022-04-14 17:34:02 +0300

    TAH-ruudun rakentelua (keskeneräinen)

--- Mökkivaraus/ToimintaAlueHallinta.cs
+++ Mökkivaraus/ToimintaAlueHallinta.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace Mökkivaraus
 {
@@ -16,6 +19,9 @@ namespace Mökkivaraus
         {
             InitializeComponent();
         }
+
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;Initial Catalog=vp;username=root;Password=Ruutti");
+        MySqlCommand command;
 
         private void tsmEtusivu_Click(object sender, EventArgs e)
         {
@@ -52,6 +58,106 @@ namespace Mökkivaraus
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToimintaAlueHallinta_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'vnDataSet.mokki' table. You can move, or remove it, as needed.
+            this.mokkiTableAdapter.Fill(this.vnDataSet.mokki);
+            // TODO: This line of code loads data into the 'vnDataSet.alue' table. You can move, or remove it, as needed.
+            this.alueTableAdapter.Fill(this.vnDataSet.alue);
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void ToimintaAlueLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gridMokkiTaulukko_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            populateDGV();
+        }
+
+        public void populateDGV()
+        {
+            string query = "SELECT * FROM mokki";
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+            adapter.Fill(table);
+            gridMokkiTaulukko.DataSource = table;
+        }
+
+
+        public void OpenConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public void ExecuteMyQuery(string query)
+        {
+            try
+            {
+                OpenConnection();
+                command = new MySqlCommand(query, connection);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Kysely suoritettu");
+                }
+                else
+                {
+                    MessageBox.Show("Kyselyä ei suoritettu");
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.mokkiTableAdapter.FillBy(this.vnDataSet.mokki);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
 
         }
     }
