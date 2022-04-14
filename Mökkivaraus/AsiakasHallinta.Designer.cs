ea6acb0896a8c1130a7b0ddd40a72701809fe7b3
Aapo Laivamaa <aapolai@uef.fi>
2022-04-14 15:19:14 +0300

    BranchDemo

--- Mökkivaraus/AsiakasHallinta.Designer.cs
+++ Mökkivaraus/AsiakasHallinta.Designer.cs
@@ -49,6 +49,7 @@ namespace Mökkivaraus
             this.vnDataSet = new Mökkivaraus.VnDataSet();
             this.asiakasTableAdapter = new Mökkivaraus.VnDataSetTableAdapters.asiakasTableAdapter();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.asiakasBindingSource)).BeginInit();
@@ -191,11 +192,21 @@ namespace Mökkivaraus
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "BranchNappi";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // AsiakasHallinta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(930, 570);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.toolStrip1);
@@ -232,5 +243,6 @@ namespace Mökkivaraus
         private System.Windows.Forms.DataGridViewTextBoxColumn puhelinnroDataGridViewTextBoxColumn;
         private System.Windows.Forms.ToolStripMenuItem tsmMokki;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
