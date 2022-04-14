34d0c07775d01df23a203c1ce77e82c07b13d76e
Joona Oikarinen <Joona.Oikarinen@edu.savonia.fi>
2022-04-14 15:33:14 +0300

    Muokattu Asiakas Hallinta ikkunaa

--- Mökkivaraus/AsiakasHallinta.Designer.cs
+++ Mökkivaraus/AsiakasHallinta.Designer.cs
@@ -48,6 +48,20 @@ namespace Mökkivaraus
             this.asiakasBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.vnDataSet = new Mökkivaraus.VnDataSet();
             this.asiakasTableAdapter = new Mökkivaraus.VnDataSetTableAdapters.asiakasTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblPostnro = new System.Windows.Forms.Label();
+            this.lblEtunimi = new System.Windows.Forms.Label();
+            this.lblSukunimi = new System.Windows.Forms.Label();
+            this.lblOsoite = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPuhnro = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.asiakasBindingSource)).BeginInit();
@@ -60,7 +74,7 @@ namespace Mökkivaraus
             this.tsdAsiakas});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(930, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1209, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -119,9 +133,9 @@ namespace Mökkivaraus
             this.emailDataGridViewTextBoxColumn,
             this.puhelinnroDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.asiakasBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(498, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(452, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(420, 546);
+            this.dataGridView1.Size = new System.Drawing.Size(745, 546);
             this.dataGridView1.TabIndex = 1;
             // 
             // asiakasidDataGridViewTextBoxColumn
@@ -180,11 +194,144 @@ namespace Mökkivaraus
             // 
             this.asiakasTableAdapter.ClearBeforeFill = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "asiakas_id", true));
+            this.textBox1.Location = new System.Drawing.Point(310, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "postinro", true));
+            this.textBox2.Location = new System.Drawing.Point(310, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "etunimi", true));
+            this.textBox3.Location = new System.Drawing.Point(310, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "email", true));
+            this.textBox4.Location = new System.Drawing.Point(310, 185);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(136, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "lahiosoite", true));
+            this.textBox5.Location = new System.Drawing.Point(310, 158);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(136, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "sukunimi", true));
+            this.textBox6.Location = new System.Drawing.Point(310, 132);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(136, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.asiakasBindingSource, "puhelinnro", true));
+            this.textBox7.Location = new System.Drawing.Point(310, 211);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(136, 20);
+            this.textBox7.TabIndex = 8;
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(221, 56);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(18, 13);
+            this.lblid.TabIndex = 9;
+            this.lblid.Text = "ID";
+            // 
+            // lblPostnro
+            // 
+            this.lblPostnro.AutoSize = true;
+            this.lblPostnro.Location = new System.Drawing.Point(221, 82);
+            this.lblPostnro.Name = "lblPostnro";
+            this.lblPostnro.Size = new System.Drawing.Size(45, 13);
+            this.lblPostnro.TabIndex = 10;
+            this.lblPostnro.Text = "Postinro";
+            // 
+            // lblEtunimi
+            // 
+            this.lblEtunimi.AutoSize = true;
+            this.lblEtunimi.Location = new System.Drawing.Point(221, 109);
+            this.lblEtunimi.Name = "lblEtunimi";
+            this.lblEtunimi.Size = new System.Drawing.Size(41, 13);
+            this.lblEtunimi.TabIndex = 11;
+            this.lblEtunimi.Text = "Etunimi";
+            // 
+            // lblSukunimi
+            // 
+            this.lblSukunimi.AutoSize = true;
+            this.lblSukunimi.Location = new System.Drawing.Point(221, 135);
+            this.lblSukunimi.Name = "lblSukunimi";
+            this.lblSukunimi.Size = new System.Drawing.Size(50, 13);
+            this.lblSukunimi.TabIndex = 14;
+            this.lblSukunimi.Text = "Sukunimi";
+            // 
+            // lblOsoite
+            // 
+            this.lblOsoite.AutoSize = true;
+            this.lblOsoite.Location = new System.Drawing.Point(221, 161);
+            this.lblOsoite.Name = "lblOsoite";
+            this.lblOsoite.Size = new System.Drawing.Size(37, 13);
+            this.lblOsoite.TabIndex = 13;
+            this.lblOsoite.Text = "Osoite";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(221, 188);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 12;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPuhnro
+            // 
+            this.lblPuhnro.AutoSize = true;
+            this.lblPuhnro.Location = new System.Drawing.Point(221, 214);
+            this.lblPuhnro.Name = "lblPuhnro";
+            this.lblPuhnro.Size = new System.Drawing.Size(77, 13);
+            this.lblPuhnro.TabIndex = 15;
+            this.lblPuhnro.Text = "Puhelinnumero";
+            // 
             // AsiakasHallinta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(930, 570);
+            this.ClientSize = new System.Drawing.Size(1209, 570);
+            this.Controls.Add(this.lblPuhnro);
+            this.Controls.Add(this.lblSukunimi);
+            this.Controls.Add(this.lblOsoite);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblEtunimi);
+            this.Controls.Add(this.lblPostnro);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "AsiakasHallinta";
@@ -219,5 +366,19 @@ namespace Mökkivaraus
         private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn puhelinnroDataGridViewTextBoxColumn;
         private System.Windows.Forms.ToolStripMenuItem tsmMokki;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblPostnro;
+        private System.Windows.Forms.Label lblEtunimi;
+        private System.Windows.Forms.Label lblSukunimi;
+        private System.Windows.Forms.Label lblOsoite;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPuhnro;
     }
 }
